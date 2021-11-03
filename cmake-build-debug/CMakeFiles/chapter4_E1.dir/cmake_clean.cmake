cb2be610b2f11000e1d6ea76e152fdd73faa3b1c
Kevin <kevin,anjorin@gmx.at>
2021-11-03 23:09:41 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/chapter4_E1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter4_E1.dir/main.c.obj"
+  "chapter4_E1.exe"
+  "chapter4_E1.exe.manifest"
+  "chapter4_E1.pdb"
+  "libchapter4_E1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter4_E1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,k;
+   /*
+    * printf("Enter 2 digit number: ");
+    scanf("%d",&i);
+    printf("The reversal is: %d ",(i%10)*10+i/10);
+    */
+   /*
+    printf("Enter 3 digit number: ");
+    scanf("%d",&i);
+    printf("The reversal is: %d ",(i%10)*100+i%100/10*10+i/100);
+    */
+   /*
+    printf("Enter 3 digit number: ");
+    scanf("%1d%1d%1d",&i,&j,&k);
+    printf("The reversal is: %1d%1d%1d ",k,j,i);
+    */
+  
+
+}
